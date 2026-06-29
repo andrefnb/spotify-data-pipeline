88d3afe1fce914b4cd2bb71377973b2239e483f5
andrefnb <andrebastos2601@gmail.com>
2026-06-29 22:20:35 +0000

    fixed linter issues

--- tests/unit/test_transformer.py
+++ tests/unit/test_transformer.py
@@ -1,7 +1,6 @@
-from datetime import datetime, timezone
+from datetime import UTC, datetime
 
 import polars as pl
-import pytest
 
 from spotify_pipeline.transformation.transformer import Transformer
 from spotify_pipeline.validation.models import PlayedAt
@@ -28,11 +27,26 @@ def test_transform_sorts_descending() -> None:
     from spotify_pipeline.validation.models import Album, Artist, Track
 
     artist = Artist(id="a1", name="A", uri="spotify:artist:a1")
-    album = Album(id="al1", name="AL", release_date="2024-01-01", total_tracks=1, uri="spotify:album:al1")
-    track = Track(id="t1", name="T", duration_ms=1000, explicit=False, popularity=50, uri="spotify:track:t1", artists=[artist], album=album)
+    album = Album(
+        id="al1",
+        name="AL",
+        release_date="2024-01-01",
+        total_tracks=1,
+        uri="spotify:album:al1",
+    )
+    track = Track(
+        id="t1",
+        name="T",
+        duration_ms=1000,
+        explicit=False,
+        popularity=50,
+        uri="spotify:track:t1",
+        artists=[artist],
+        album=album,
+    )
 
-    older = PlayedAt(track=track, played_at=datetime(2024, 1, 1, tzinfo=timezone.utc))
-    newer = PlayedAt(track=track, played_at=datetime(2024, 6, 1, tzinfo=timezone.utc))
+    older = PlayedAt(track=track, played_at=datetime(2024, 1, 1, tzinfo=UTC))
+    newer = PlayedAt(track=track, played_at=datetime(2024, 6, 1, tzinfo=UTC))
 
     df = Transformer().transform([older, newer])
     assert df["played_at"][0] == newer.played_at
